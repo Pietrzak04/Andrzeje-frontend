ec3fd70604ef9b32837b9620587782b6c3789c31
Julia Korzekwa <julia0n3@gmail.com>
2022-12-03 00:30:53 +0100

    added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BottomNavBarComponent } from './bottom-nav-bar/bottom-nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { EscapePointsMapComponent } from './escape-points-map/escape-points-map.component';
+import { ChallengeMapComponent } from './challenge-map/challenge-map.component';
+import { AppRoutingModule } from './routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomNavBarComponent,
+    EscapePointsMapComponent,
+    ChallengeMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
