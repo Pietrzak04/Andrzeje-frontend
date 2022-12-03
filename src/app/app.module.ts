647ca17db8cf935e3fa0b6784b9f7434a1ff9f36
Dominik <dominik.switlik@gmail.com>
2022-12-03 02:04:59 +0100

    voting.component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BottomNavBarComponent } from './bottom-nav-bar/bottom-nav-bar.component';
-import { RouterModule } from '@angular/router';
 import { EscapePointsMapComponent } from './escape-points-map/escape-points-map.component';
 import { ChallengeMapComponent } from './challenge-map/challenge-map.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { VotingComponent } from './voting/voting.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     BottomNavBarComponent,
     EscapePointsMapComponent,
     ChallengeMapComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    VotingComponent
   ],
   imports: [
     BrowserModule,
