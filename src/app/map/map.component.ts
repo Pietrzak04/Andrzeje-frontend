d301c59b2868c657e6b23dfcf6e777c4670ac35e
Julia Korzekwa <julia0n3@gmail.com>
2022-12-03 04:11:48 +0100

    added markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,12 +1,27 @@
 import { Component, AfterViewInit  } from '@angular/core';
 import * as L from 'leaflet';
-
+import { EscapePointsService } from '../data-services/escape-points.service';
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit{
+constructor(private escapePointsService: EscapePointsService) {}
 
   private map: any;
 
@@ -23,6 +38,13 @@ export class MapComponent implements AfterViewInit{
     });
 
     tiles.addTo(this.map);
+
+    let escapePoints = this.escapePointsService.getEscapePoints();
+
+    escapePoints.forEach(value => {
+      const marker = L.marker([value.latitude, value.longitude]);
+      marker.addTo(this.map);
+    })
   }
 
   ngAfterViewInit() {
